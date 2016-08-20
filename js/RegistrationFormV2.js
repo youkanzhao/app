94ac9e622f53d943c661815a73fd56702e3382e2
june <youkanzhao@gmail.com>
2016-08-20 14:16:57 +0800

    add code

---
+++ js/RegistrationFormV2.js
@@ -0,0 +1,151 @@
+﻿///////////////////////////////////////////////////////////////////////
+/// GLOBAL VARIABLES - PLEASE SET THESE ACCORDING TO IMPLEMENTATION ///
+///////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////
+/// On version 2.0, we removed all drop down initialization and populate it from API datasource from cdn.cbresi.com.au which retrieves data from Eloqua REST API
+///////////////////////////////////////////////////////////////////////
+
+//var domainName = 'lucent.ondicomdigital.com'; // pulled from iframe query string - make static for individual sites
+var styleUrl = 'http://' + domainName + '/css/style.css'; // built from domainName, can be statically set depending on needs
+var projId = jQuery("#projectID").val(); // Salesforce Project ID
+var thankYouPage = jQuery("#thankYouPage").val(); //Full URL to Thank You page for Eloqua to redirect to after successful form submission
+
+
+// ELOQUA DATA LOOKUP:
+var eloquaSiteId = '952923355'; // Eloqua Site ID (same as form)
+var eloquaFormName = jQuery("#elqFormName").val(); // Eloqua Form Name (same as form)
+
+///////////////////////////////////////////////////////////////////////
+/// END GLOBAL VARIABLES //////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+
+// Initial setup for Eloqua Data Lookup.
+var _elqQ = _elqQ || [];
+_elqQ.push(['elqSetSiteId', eloquaSiteId]);
+_elqQ.push(['elqTrackPageView']);
+
+(function () {
+    function async_load() {
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+        s.src = 'http://img.en25.com/i/elqCfg.min.js';
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    }
+    if (window.addEventListener) window.addEventListener('DOMContentLoaded', async_load, false);
+    else if (window.attachEvent) window.attachEvent('onload', async_load);
+    updatePreferredBuyerType();
+    updatePreferredPropertyType();
+})();
+
+function validateForm() {
+    if (jQuery('input.buyerType:checked').length==0) {
+        jQuery('input.buyerType').parent("p").addClass('parsley-error');
+        return false;
+    }
+    if (jQuery('input.PreferredPropertyType:checked').length == 0) {
+        jQuery('input.PreferredPropertyType').parent("p").addClass('parsley-error');
+        return false;
+    }
+}
+
+$('.buyerType').click(function () { 
+    updatePreferredBuyerType();
+});
+$('.PreferredPropertyType').click(function () {
+    updatePreferredPropertyType();
+});
+function updatePreferredBuyerType() {
+    var selectedvalues = "";
+    $('input.buyerType:checked').each(function () {
+        if (selectedvalues == "")
+            selectedvalues = this.value;
+        else
+            selectedvalues = selectedvalues + "::" + this.value;
+    });
+    jQuery("#delimitedBuyerType").val(selectedvalues)
+}
+function updatePreferredPropertyType() {
+    var selectedvalues = "";
+    $('input.PreferredPropertyType:checked').each(function () {
+        if (selectedvalues == "")
+            selectedvalues = this.value;
+        else
+            selectedvalues = selectedvalues + "::" + this.value;
+    });
+    
+    jQuery("#delimitedPreferredPropertyType").val(selectedvalues)
+}
+// Initial variable creation and pre-population
+var elqpush = '<Project_ID1>' + projId + '</Project_ID1><Language1>' + pageLang + '</Language1>';
+
+
+// Initial function executed onload
+function doInitial() {
+    // Fill hidden form fields
+
+    jQuery("#thankYouPage").val(thankYouPage);
+    jQuery("#projectID").val(projId);
+    /*
+    document.getElementById("utmSource").value = utmSource;
+    document.getElementById("utmMedium").value = utmMedium;
+    document.getElementById("utmTerm").value = utmTerm;
+    document.getElementById("utmContent").value = utmContent;
+    document.getElementById("utmCampaign").value = utmCampaign;
+    */
+    // Assign parent frame style to iframe
+    //$('head').append($('<link rel="stylesheet" type="text/css" />').attr('href', styleUrl));
+
+    // Do Chinese translation if required
+    if (pageLang == 'CH' || pageLang == 'ch') {
+        translateToCh();
+    }
+
+
+    // Unhide after loading
+    jQuery("body").removeClass("hideme");
+}
+
+// Eloqua Data Lookup function
+function SetElqContent() {
+    if (this.GetElqContentPersonalizationValue) {
+
+        var elqForm;
+        var blnFound = false;
+        //find the form object from the form HTMLName
+        for (var formIndex = 0; formIndex < document.forms.length; formIndex++) {
+            for (var elemIndex = 0; elemIndex < document.forms[formIndex].length; elemIndex++) {
+                if (document.forms[formIndex].elements[elemIndex].name == "elqFormName") {
+                    if (document.forms[formIndex].elements[elemIndex].value == eloquaFormName) {
+                        elqForm = document.forms[formIndex];
+                        blnFound = true;
+                        break;
+                    }
+                }
+            }
+            if (blnFound) break;
+        }
+
+    }
+}
+
+// Creates EloquaID for the visitor
+function generateElqId() {
+    var eloquaID = (jQuery("#emailAddress").val().toLowerCase() + jQuery("#projectID").val());
+    jQuery("#eloquaID").val(eloquaID);
+}
+
+
+
+// Function to modify all form labels and headings to Chinese versions
+function translateToCh() {
+    jQuery('label_heading').html('&#27442;&#33719;&#24471;&#26356;&#35814;&#23613;&#30340;&#20449;&#24687;&#25110;&#32773;&#27004;&#23618;&#22270;&#65292;&#35831;&#22635;&#20889;&#20197;&#19979;&#34920;&#26684;&#12290;(&#35831;&#29992;&#33521;&#25991;)&#12290;');
+    jQuery('label_fname').html('&#21517;*');
+    jQuery('label_lname').html('&#22995;*');
+    jQuery('label_phone').html('&#30005;&#35805;*');
+    jQuery('label_email').html('&#30005;&#23376;&#37038;&#20214;*');
+    jQuery('label_postcode').html('&#37038;&#32534;*');
+    jQuery('label_howdidyouhear').html('&#24744;&#26159;&#22914;&#20309;&#24471;&#30693;&#25105;&#20204;&#30340;&#39033;&#30446;*');
+    jQuery('label_mandatory').html('*&#24517;&#22635;&#39033;&#30446;');
+    jQuery("#field5 option:first").html('&#24744;&#26159;&#22914;&#20309;&#24471;&#30693;&#25105;&#20204;&#30340;&#39033;&#30446;*')
+}
---
+++ js/watermarkify.js
@@ -0,0 +1,84 @@
+﻿/*
+* Watermarkify - jQuery Plugin
+* A simple animated form field watermarking plugin
+*
+* Examples and documentation at: http://www.ajaybalachandran.com/blog
+*
+* Copyright (c)2011 Ajay Balachandran
+* The CSS file included with this package needs to be linked with the html via a link tag in order for the plugin to work properly.
+*
+*/
+(function (a) { a.fn.extend({ watermarkify: function (b) { var c = { time1: 200, time2: 200, easing: "" }; var b = a.extend(c, b); if (!!(window.ActiveXObject)) { } return this.each(function () { var f = b; var e = a(this); var d = e.attr("placeholder"); e.wrap("<div class='watermarkify-wrap' />"); a("<span class='watermarkify-watermark'><span class='watermarkify-watermark-inner'>" + d + "</span></span>").insertAfter(e); fieldHeight = e.outerHeight(); waterMarkHeight = e.next().outerHeight(); waterMarkTop = (fieldHeight - waterMarkHeight) / 2; e.next(".watermarkify-watermark").css({ top: waterMarkTop }); if (e.val() != "") { if (a.browser.msie) { e.parent().find(".watermarkify-watermark-inner").hide() } else { e.parent().find(".watermarkify-watermark-inner").css({ "margin-left": "-40px", opacity: "0" }) } } e.focus(function (g) { e.parent().find(".watermarkify-watermark").addClass("watermarkify-watermark-blurred") }); e.keyup(function (g) { if (a.browser.msie) { e.parent().find(".watermarkify-watermark-inner").hide() } else { e.parent().find(".watermarkify-watermark-inner").animate({ "margin-left": "-40px", opacity: "0" }, f.time1, f.easing) } }); e.blur(function (g) { if (e.val() == "") { e.parent().find(".watermarkify-watermark-blurred").removeClass("watermarkify-watermark-blurred"); if (a.browser.msie) { e.parent().find(".watermarkify-watermark-inner").show() } else { e.parent().find(".watermarkify-watermark-inner").animate({ "margin-left": "0", opacity: "1" }, f.time2, f.easing) } } }); a(".watermarkify-watermark").click(function () { a(this).prev().focus() }) }) } }) })(jQuery);
+
+var matched, browser;
+
+jQuery.uaMatch = function (ua) {
+    ua = ua.toLowerCase();
+
+    var match = /(chrome)[ \/]([\w.]+)/.exec(ua) ||
+        /(webkit)[ \/]([\w.]+)/.exec(ua) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua) ||
+        /(msie) ([\w.]+)/.exec(ua) ||
+        ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) ||
+        [];
+
+    return {
+        browser: match[1] || "",
+        version: match[2] || "0"
+    };
+};
+
+matched = jQuery.uaMatch(navigator.userAgent);
+browser = {};
+
+if (matched.browser) {
+    browser[matched.browser] = true;
+    browser.version = matched.version;
+}
+
+// Chrome is Webkit, but Webkit is also Safari.
+if (browser.chrome) {
+    browser.webkit = true;
+} else if (browser.webkit) {
+    browser.safari = true;
+}
+
+
+//Check IE Version
+var ie = (function () {
+    var undef,
+                    v = 3,
+                    div = document.createElement('div'),
+                    all = div.getElementsByTagName('i');
+    while (
+                    div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+                    all[0]
+                );
+    return v > 4 ? v : undef;
+} ());
+
+/*
+    (function ($) {
+         $.support.placeholder = ('placeholder' in document.createElement('input'));
+     })(jQuery);
+
+
+     //fix for IE7 and IE8
+     $(function () {
+         if (!$.support.placeholder) {
+             $("[placeholder]").focus(function () {
+                 if ($(this).val() == $(this).attr("placeholder")) $(this).val("");
+             }).blur(function () {
+                 if ($(this).val() == "") $(this).val($(this).attr("placeholder"));
+             }).blur();
+
+             $("[placeholder]").parents("form").submit(function () {
+                 $(this).find('[placeholder]').each(function() {
+                     if ($(this).val() == $(this).attr("placeholder")) {
+                         $(this).val("");
+                     }
+                 });
+             });
+         }
+     });
+ */
